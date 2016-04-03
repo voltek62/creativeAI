43c0235be5039d05a7fa3d5641cfc6da7b881a62
samim23 <s@samim.io>
2016-04-04 01:57:41 +0200

    video inline

--- custom.js
+++ custom.js
@@ -65,6 +65,35 @@ if(Meteor.isClient){
 
 
   /// HELPERS ///
+  function convertMedia(html){
+    var pattern1 = /(?:http?s?:\/\/)?(?:www\.)?(?:vimeo\.com)\/?(.+)/g;
+    var pattern2 = /(?:http?s?:\/\/)?(?:www\.)?(?:youtube\.com|youtu\.be)\/(?:watch\?v=)?(.+)/g;
+    var pattern3 = /([-a-zA-Z0-9@:%_\+.~#?&//=]{2,256}\.[a-z]{2,4}\b(\/[-a-zA-Z0-9@:%_\+.~#?&//=]*)?(?:jpg|jpeg|gif|png))/gi;
+
+    if(pattern1.test(html)){
+       var replacement = '<iframe width="420" height="345" src="//player.vimeo.com/video/$1" frameborder="0" webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe>';
+       var html = html.replace(pattern1, replacement);
+    }
+
+    if(pattern2.test(html)){
+      var replacement = '<iframe width="420" height="345" src="http://www.youtube.com/embed/$1" frameborder="0" allowfullscreen></iframe>';
+      var html = html.replace(pattern2, replacement);
+    } 
+
+    if(pattern3.test(html)){
+      var replacement = '<a href="$1" target="_blank"><img class="sml" src="$1" /></a><br />';
+      var html = html.replace(pattern3, replacement);
+    }     
+    return html;
+  }
+
+  Template.registerHelper("makeVideo", function (object) {
+    var convert = convertMedia(this.body);
+    var ret = Telescope.utils.sanitize(marked(convert));
+    return ret;
+  });
+
+
 	Template.registerHelper("isEmpty", function (object) {
 		if(object === undefined){return false;}
     return true;
